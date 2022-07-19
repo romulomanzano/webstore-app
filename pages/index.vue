20f2b953dc2b8ab25e048917bcc391867f444621
romulomanzano <romulomanzano@gmail.com>
2022-07-19 16:48:32 -0400

    Changes to support store settings

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,12 @@
     <v-row class="justify-center"
       ><notifications group="notifications" position="top"
     /></v-row>
-    <v-tabs background-color="primary" v-model="tab" center-active dark>
+    <v-tabs background-color="primary" v-model="tab" center-active show-arrows dark>
       <v-tab>General</v-tab>
-      <v-tab :disabled="activeStore === null">Links</v-tab>
+      <v-tab :disabled="activeStore === null">Enlaces</v-tab>
+      <v-tab :disabled="activeStore === null">Contacto</v-tab>
+      <v-tab :disabled="activeStore === null">Envio</v-tab>
+      <v-tab :disabled="activeStore === null">Personalizacion</v-tab>
     </v-tabs>
     <v-tabs-items v-model="tab">
       <v-tab-item>
