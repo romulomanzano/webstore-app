04f90dc50995b835edde572d18ffafb16c261f23
romulomanzano <romulomanzano@gmail.com>
2022-07-20 14:08:40 -0400

    Adding validator

--- components/store/ContactTab.vue
+++ components/store/ContactTab.vue
@@ -7,6 +7,9 @@
         :error-messages="invalidWhatsapp"
         @blur="$v.storeDetails.whatsapp.$touch()"
         prepend-icon="mdi-whatsapp"
+        hint="Utiliza el simbolo +, seguido por codigo de pais, y numero. Ejemplo: +18887775555"
+        persistent-hint
+        class="mb-2"
       >
       </v-text-field>
       <v-btn class="mr-4 mb-2 mt-2" @click="cancelUpdate"> Cancelar </v-btn>
@@ -22,8 +25,7 @@ import { helpers } from "vuelidate/lib/validators";
 import { validationMixin } from "vuelidate";
 import { mapGetters, mapActions } from "vuex";
 
-const phoneValidator = (value) =>
-  helpers.regex(value, /^\+?\d{0,3}[\(\- ]?\d{3}\)?[\- ]?\d{3,4}[\- ]?\d{4}/);
+const phoneValidator = helpers.regex('phoneValidator', /^\+(9[976]\d|8[987530]\d|6[987]\d|5[90]\d|42\d|3[875]\d|2[98654321]\d|9[8543210]|8[6421]|6[6543210]|5[87654321]|4[987654310]|3[9643210]|2[70]|7|1)\d{1,14}$/);
 
 export default {
   name: "ContactTab",
