29ba94efaf148d44a81b200b1d72cf966ec16a00
romulomanzano <romulomanzano@gmail.com>
2022-07-25 08:33:25 -0400

    Changes to logo uploader, etc

--- components/store/BrandTab.vue
+++ components/store/BrandTab.vue
@@ -1,27 +1,45 @@
 <template>
   <div>
     <v-form class="ma-4" ref="form" lazy-validation>
-      <v-img
-        :max-height="128"
-        :max-width="128"
-        :src="storeDetails.logoResized?.dataUrl"
-      ></v-img>
-      <image-uploader
-        @input="setImage"
-        :debug="1"
-        :maxWidth="128"
-        :maxHeight="128"
-        :quality="0.9"
-        :autoRotate="true"
-        outputFormat="verbose"
-        :preview="false"
-        :className="['fileinput', { 'fileinput--loaded': hasImage }]"
-        :capture="true"
-        accept="image/*"
-        doNotResize="['gif', 'svg']"
-        @onComplete="info"
-      >
-      </image-uploader>
+      <h3>Logo</h3>
+      <template>
+        <v-img
+          class="mt-4"
+          :max-height="128"
+          :max-width="128"
+          :src="storeDetails.logo?.dataUrl"
+        ></v-img>
+        <image-uploader
+          @input="setImage"
+          :debug="1"
+          :maxWidth="128"
+          :maxHeight="128"
+          :quality="0.9"
+          :autoRotate="true"
+          outputFormat="verbose"
+          :preview="false"
+          :className="['fileinput', { 'fileinput--loaded': hasImage }]"
+          :capture="true"
+          accept="image/*"
+          doNotResize="['gif', 'svg']"
+          class="mt-4"
+        >
+        </image-uploader>
+      </template>
+      <h3 class="mt-4">Color Principal</h3>
+      <v-color-picker
+        v-model="storeDetails.primaryColor"
+        hide-canvas
+        mode="hexa"
+        hide-mode-switch
+      ></v-color-picker>
+      <h3 class="mt-4">Color Secundario</h3>
+      <v-color-picker
+        v-model="storeDetails.secondaryColor"
+        hide-canvas
+        mode="hexa"
+        hide-mode-switch
+      ></v-color-picker>
       <v-btn class="mr-4 mb-2 mt-2" @click="cancelUpdate"> Cancelar </v-btn>
       <v-btn class="mb-2 mt-2" :disabled="!isValidForm" @click="saveStore">
         Guardar
@@ -31,30 +49,24 @@
 </template>
 
 <script>
-import { helpers, required } from "vuelidate/lib/validators";
-import { validationMixin } from "vuelidate";
 import { mapGetters, mapActions } from "vuex";
 import ImageUploader from "vue-image-upload-resize";
 
 export default {
   name: "BrandTab",
-  mixins: [validationMixin],
   components: { ImageUploader },
   data: () => ({
     hasImage: false,
     storeDetails: {
       logo: null,
-      logoResized: null,
-    },
-    basestoreDetails: {
-      logo: null,
-      logoResized: null,
+      primayColor: null,
+      secondaryColor: null,
     },
     logoRules: [
       (value) =>
         !value ||
         value.size < 2000000 ||
-        "Avatar size should be less than 2 MB!",
+        "El archivo debe tener un tamaño menor de 2 MB!",
     ],
   }),
   watch: {
@@ -66,11 +78,17 @@ export default {
     ...mapActions({ updateStore: "updateStore" }),
     setImage: function (file) {
       this.hasImage = true;
-      this.storeDetails.logoResized = file;
+      this.storeDetails.logo = file;
+      this.storeDetails.logo.info.modifiedDate = null; //needed to avoid upload errors
     },
     resetSettings() {
       if (this.activeStore.brandSettings === undefined) {
-        this.storeDetails = Object.assign({}, this.baseStoreDetails);
+        const settings = {
+          logo: null,
+          primaryColor: this.$vuetify.theme.themes.dark.primary,
+          secondaryColor: this.$vuetify.theme.themes.dark.secondary,
+        };
+        this.storeDetails = Object.assign({}, settings);
       } else {
         this.storeDetails = Object.assign({}, this.activeStore.brandSettings);
       }
@@ -87,9 +105,6 @@ export default {
           group: "notifications",
         });
       });
-    },
-    info() {
-      console.log(this.storeDetails.logoResized);
     },
   },
   computed: {
