ea357e6548cb1833f2d29780429192bca7db6794
romulomanzano <romulomanzano@gmail.com>
2022-08-10 07:23:20 -0400

    clean up

--- components/product/ProductInventoryTab.vue
+++ components/product/ProductInventoryTab.vue
@@ -4,7 +4,7 @@
       <v-radio-group
         v-model="productDetails.hasVariations"
         @blur="$v.productDetails.hasVariations.$touch()"
-        hint="Elegir si el producto viene en distintas presentaciones (variaciones).
+        hint="Elegir si el producto viene en distintas presentaciones (variantes).
           Por ejemplo, talla, color, tamaño, etc."
         persistent-hint
       >
@@ -20,72 +20,93 @@
       <h4 class="ma-2 mt-4 font-weight-medium">Variantes del Producto</h4>
       <v-list>
         <v-list-group
-          v-for="item in items"
-          :key="item.title"
-          v-model="item.active"
+          v-for="category in categories"
+          :key="category.title"
+          v-model="category.active"
           no-action
         >
           <template v-slot:activator>
             <v-list-item-content>
-              <v-list-item-title v-text="item.title"></v-list-item-title>
+              <v-list-item-title v-text="category.title"></v-list-item-title>
             </v-list-item-content>
           </template>
-          <v-list-item> </v-list-item>
-
-          <v-list-item v-for="child in item.items" :key="child.key">
-            <v-list-item-content>
-              <v-card outlined rounded elevation="2" class="pa-2">
-                <v-row>
-                  <v-col>
-                    <v-text-field
-                      v-model="child.title"
-                      filled
-                      label="Opcion"
-                      type="text"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col>
-                    <v-text-field
-                      v-model="child.stock"
-                      filled
-                      label="# disponibles"
-                      type="number"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col>
-                    <v-text-field
-                      v-model="child.additionalCost"
-                      filled
-                      label="Costo adicional"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-card-actions>
-                  <v-btn
-                    text
-                    color="primary"
-                    @click="removeOption(item, child)"
-                  >
-                    Eliminar
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-btn
-              class="mx-2"
-              fab
-              dark
-              small
-              color="primary"
-              @click="addOption(item)"
+          <div>
+            <v-data-table
+              :headers="headers"
+              :items="desserts"
+              :disable-sort="true"
             >
-              <v-icon dark> mdi-plus </v-icon>
-            </v-btn>
-          </v-list-item>
+              <template v-slot:item.name="props">
+                <v-edit-dialog
+                  :return-value.sync="props.item.name"
+                  @save="save"
+                  @cancel="cancel"
+                  @open="open"
+                  @close="close"
+                >
+                  {{ props.item.name }}
+                  <template v-slot:input>
+                    <v-text-field
+                      v-model="props.item.name"
+                      label="Editar"
+                      single-line
+                      counter
+                    ></v-text-field>
+                  </template>
+                </v-edit-dialog>
+              </template>
+              <template v-slot:item.stock="props">
+                <v-edit-dialog
+                  :return-value.sync="props.item.stock"
+                  @save="save"
+                  @cancel="cancel"
+                  @open="open"
+                  @close="close"
+                >
+                  <div>{{ props.item.stock }}</div>
+                  <template v-slot:input>
+                    <v-text-field
+                      v-model="props.item.stock"
+                      label="Editar"
+                      single-line
+                      counter
+                      autofocus
+                    ></v-text-field>
+                  </template>
+                </v-edit-dialog>
+              </template>
+              <template v-slot:item.addOnCost="props">
+                <v-edit-dialog
+                  :return-value.sync="props.item.addOnCost"
+                  @save="save"
+                  @cancel="cancel"
+                  @open="open"
+                  @close="close"
+                >
+                  <div>{{ props.item.addOnCost }}</div>
+                  <template v-slot:input>
+                    <v-text-field
+                      v-model="props.item.addOnCost"
+                      label="Editar"
+                      single-line
+                      counter
+                      autofocus
+                    ></v-text-field>
+                  </template>
+                </v-edit-dialog>
+              </template>
+            </v-data-table>
+
+            <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
+              {{ snackText }}
+
+              <template v-slot:action="{ attrs }">
+                <v-btn v-bind="attrs" text @click="snack = false">
+                  Close
+                </v-btn>
+              </template>
+            </v-snackbar>
+          </div>
         </v-list-group>
       </v-list>
       <div class="mt-4 mb-2">
@@ -107,6 +128,32 @@ export default {
   name: "ProductInventoryTab",
   mixins: [validationMixin],
   data: () => ({
+    snack: false,
+    snackColor: "",
+    snackText: "",
+    max25chars: (v) => v.length <= 25 || "Input too long!",
+    pagination: {},
+    headers: [
+      {
+        text: "Opcion",
+        align: "start",
+        value: "name",
+      },
+      { text: "Stock", value: "stock" },
+      { text: "Costo Adicional", value: "addOnCost" },
+    ],
+    desserts: [
+      {
+        name: "Frozen Yogurt",
+        stock: 159,
+        addOnCost: 4.0,
+      },
+      {
+        name: "Ice cream sandwich",
+        stock: 237,
+        addOnCost: 4.3,
+      },
+    ],
     productDetails: {
       hasVariations: null,
       variations: null,
@@ -119,7 +166,7 @@ export default {
       { value: false, label: "No, presentación única" },
       { value: true, label: "Sí, tiene variantes" },
     ],
-    items: [
+    categories: [
       {
         items: [{ title: "List Item", key: "f", additionalCost: 3, stock: 2 }],
         title: "Attractions",
@@ -186,6 +233,24 @@ export default {
           group: "notifications",
         });
       });
+    },
+    save() {
+      this.snack = true;
+      this.snackColor = "success";
+      this.snackText = "Data saved";
+    },
+    cancel() {
+      this.snack = true;
+      this.snackColor = "error";
+      this.snackText = "Canceled";
+    },
+    open() {
+      this.snack = true;
+      this.snackColor = "info";
+      this.snackText = "Dialog opened";
+    },
+    close() {
+      console.log("Dialog closed");
     },
   },
   computed: {
